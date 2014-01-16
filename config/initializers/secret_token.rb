49bb881c599c015fc1198602b9708c5dbaaceac2
James Labocki <jameslabocki@gmail.com>
2014-01-16 18:56:05 -0500

    two

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palette::Application.config.secret_token = '04cd305d98da50c202f78f3acebbf43410c159e8e6ca8d5044fd02ec0c8c3e64a32fccd07ff1e4b561427f3ed5251f2a0765a5a0d78b79969a9ac800ab9efa21'
